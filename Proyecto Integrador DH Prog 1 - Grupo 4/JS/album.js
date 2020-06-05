028f0c2bcf1a41d031e8dcc0abc3aa25ea0c0698
franseidman <fseidman@udesa.edu.ar>
2020-06-05 18:01:39 -0300

    avanzo con api album

--- Proyecto Integrador DH Prog 1 - Grupo 4/JS/album.js
+++ Proyecto Integrador DH Prog 1 - Grupo 4/JS/album.js
@@ -1,13 +1,21 @@
 var queryString = window.location.search;
 var queryStringObj = new URLSearchParams(queryString);
 let albumid = queryStringObj.get("id")
-console.log(albumid)
+
 fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/album/" + albumid)
     .then(function(respuesta){
         return respuesta.json();
     })
     .then(function(datos){
         console.log(datos)
+        let contenedorname = document.querySelector('.albumName');
+        contenedorname.innerHTML +=  `${datos.title}`;
+        let contenedorartista = document.querySelector('.artistName');
+        contenedorartista.innerHTML +=  `${datos.artist.name}`;
+        let contenedorimg = document.querySelector('#imageArtist');
+        contenedorimg.innerHTML += `<img src="${datos.album.cover}" id="track-image">`;
+        let contenedordate = document.querySelector('.genreName');
+        contenedordate.innerHTML += `${datos.album.title}`+" - "+`${datos.album.release_date}`;
     })
     .catch(function(error){ 
         console.log(error)
